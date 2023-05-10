f5155b964327d013c513f1c629e3eb12a9a93c74
ks0510 <kshaikh051999@gmail.com>
2023-05-10 18:16:06 +0530

    UC4

--- demo/Demo1.java
+++ demo/Demo1.java
@@ -3,8 +3,9 @@ package com.demo;
 public class Demo1 {
 
 	public static void main(String[] args) {
-		
-		System.out.println("Hello");
+		int a=6,b=3,c;
+		 c= a+b;
+		System.out.println(c);
 
 	}
 
